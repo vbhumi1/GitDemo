b96c797ea518037999aa6a70c45ea5734d3dc7da
vijay <vbhumi.py@gmail.com>
2020-12-07 15:54:13 +0530

    final commit devlopment team

--- test_e2e.py
+++ test_e2e.py
@@ -51,3 +51,17 @@ class TestOne(BaseClass):
         log.info("Text received from application is "+textMatch)
 
         assert ("Success! Thank you!" in textMatch)
+	
+	def git_commands(self):
+		pass
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
